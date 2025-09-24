489ad51ecd3480f4b7d41ae950f8db386f6c8f23
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-09-23 22:35:34 -0500

    se cambia la imagen con el boton usando la funcion saludar

--- EjercicioGit/ejercicio.js
+++ EjercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
+    let cmpImagensaludo;
+    cmpImagensaludo = document.getElementById("imgsaludo");
+    cmpImagensaludo.src = "./Imagenes/gif_ganar2.gif";
     console.log("Ingresa a saludar");
 }
