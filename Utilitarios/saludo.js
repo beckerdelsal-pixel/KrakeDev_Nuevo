3b4f60e75c5464522f5eebec1b31c673a0989f1e
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-09-27 20:43:00 -0500

    Se agrega las funciones recuperarInt y recuperarFloat

--- Utilitarios/saludo.js
+++ Utilitarios/saludo.js
@@ -3,6 +3,10 @@ saludar = function(){
     nombre = recuperarTexto("txtNombre");
     let apellido;
     apellido = recuperarTexto("txtApellido");
+    let edad;
+    edad = recuperarInt("txtEdad");
+    let estatura;
+    estatura = recuperarFloat("txtEstatura");
 }
 
 recuperarTexto = function(idComponente){
@@ -12,4 +16,18 @@ recuperarTexto = function(idComponente){
     contenido = cmpTexto.value;
     console.log(contenido);
     return contenido;
+}
+
+recuperarInt = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFloat = parseFloat(valorCaja);
+    
+    return valorFloat;
 }
