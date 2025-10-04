8658f72ed91efb7a79a2e81ebf817aaaa04be49f
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-10-03 22:05:15 -0500

    Se agrega un tag para mostrar un gif segun se cumpla la condicion si es mayor a 7 exito, caso contrario fracaso

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,4 +5,9 @@ calcularPromedioNotas = function(){
     let promedio = calcularPromedio(nota1, nota2, nota3);
     let promedioF = promedio.toFixed(2);
     cambiarTexto("lblResultado",promedioF);
+    if(promedioF > 7){
+        cambiarImagen("imgResultado", "./exito.gif");
+    }else{
+        cambiarImagen("imgResultado", "./fracaso.gif");
+    }
 }
