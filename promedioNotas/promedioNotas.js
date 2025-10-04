74f4fe4b559f9e9a78e5e9e8dbdc7309a687e845
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-10-03 21:54:35 -0500

    Se crean los archivos y codigo, la pagina funciona correctamente al agregar un boton que invoque a calcularPromedioNotas

---
+++ promedioNotas/promedioNotas.js
@@ -0,0 +1,8 @@
+calcularPromedioNotas = function(){
+    let nota1 = recuperarFlotante("txtN1");
+    let nota2 = recuperarFlotante("txtN2");
+    let nota3 = recuperarFlotante("txtN3");
+    let promedio = calcularPromedio(nota1, nota2, nota3);
+    let promedioF = promedio.toFixed(2);
+    cambiarTexto("lblResultado",promedioF);
+}
