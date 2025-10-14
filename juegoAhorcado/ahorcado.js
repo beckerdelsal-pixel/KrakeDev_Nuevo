bddd6c6585d22fcdacfea104932ad5a8f70df5e8
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-10-13 23:31:00 -0500

    Paso 1, completado

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,10 +1,37 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-esMayuscula = function(caracter){
+let palabraSecreta;
+esMayuscula = function (caracter) {
     let codigo;
-    codigo = caracter.chatCodeAt(0);
-    if(codigo >= 65 && codigo <= 90){
+    codigo = caracter.charCodeAt(0);
+    if (codigo >= 65 && codigo <= 90) {
         return true;
-    }else{
+    } else {
         return false;
+    }
+}
+guardarPalabra = function () {
+    let texto;
+    let confirmacionM = true;
+    texto = recuperarTexto("txtSecreta");
+    for (let i = 0; i < texto.length; i++) {
+        let actual = texto.charAt(i);
+        if (texto.length !=5 || esMayuscula(actual) === false) {
+            confirmacionM = false;
+            
+        }
+    }
+    if(confirmacionM === true){
+        palabraSecreta = texto;
+        console.log(palabraSecreta);
+    }else{
+        alert("DEBE INGRESAR UNA PALABRA CON 5 LETRAS MAYUSCULAS");
+    }
+    cambiarAPassword("txtSecreta")
+
+}
+cambiarAPassword = function(idComponente){
+    let componente= document.getElementById(idComponente);
+    if(componente.type === 'text'){
+        componente.type = 'password';
     }
 }
