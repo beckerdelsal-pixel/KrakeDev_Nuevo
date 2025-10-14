3b8a28c3681e380cfbe42df588a065fdd220f6f2
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-10-14 00:43:06 -0500

    Completados todos los pasos, juego terminado

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,5 +1,8 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 let palabraSecreta;
+let intentos = 0;
+let coincidencias = 0;
+let errores =0;
 esMayuscula = function (caracter) {
     let codigo;
     codigo = caracter.charCodeAt(0);
@@ -50,18 +53,54 @@ validar = function(letra){
         if(letra === palabraSecreta.charAt(i)){
             mostrarLetra(letra,i);
             letrasEncontradas++;
+            coincidencias = coincidencias +1;
         }
+    }
+    if(letrasEncontradas===0){
+        alert("LA LETRA NO ES PARTE DE LA PALABRA");
+        errores++;
+        mostrarAhorcado();
     }
 }
 
 ingresarLetra = function(){
     let recuperado = recuperarTexto("txtLetra");
+    intentos++;
+    //console.log(intentos);
     if(esMayuscula(recuperado)===false){
         alert("SOLO SE ACEPTAN MAYUSCULAS")
     }else{
         validar(recuperado);
+        
     }
+    if(coincidencias === 5 && intentos <10){
+     //   alert("HA GANADO EL JUEGO!!!!");
+        mostrarImagen("ahorcadoImagen","ganador.gif");
+    }else if(intentos===10 && coincidencias <5){
+      //  alert("HA PERDIDO EL JUEGO :'c");
+        mostrarImagen("ahorcadoImagen","./gameOver.gif");
+    }
+   // console.log(coincidencias);
 }
 
-
-    
+mostrarAhorcado = function(){
+    if(errores === 1){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_01.png");
+    }else if(errores === 2){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_02.png");
+    }else if(errores === 3){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_03.png");
+    }else if(errores === 4){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_04.png");
+    }else if(errores === 5){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_05.png");
+    }else if(errores === 6){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_06.png");
+    }else if(errores === 7){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_07.png");
+    }else if(errores === 8){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_08.png");
+    }else if(errores === 9){
+        mostrarImagen("ahorcadoImagen","./Ahorcado_09.png");
+    }
+}
