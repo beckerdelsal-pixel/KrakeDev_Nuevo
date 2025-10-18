f87bfbe2de3ffa13748f7d01f73b397828d4f860
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-10-17 20:51:50 -0500

    actualiacion de archivos del directorio

---
+++ arreglos/frutas.js
@@ -0,0 +1,24 @@
+let frutas = ["pera","manzana","banana"];
+
+probarBusqueda = function(){
+    let frutaIngresada = recuperarTexto("lblFruta");
+    let resultado = buscar(frutaIngresada)
+    if(resultado == null){
+            alert("No existe la fruta");
+    }else{
+        alert(frutaIngresada+" existe en el cesto!");
+    }
+}
+
+buscar = function(fruta){
+    let elemento;
+    let FrutaEncontrada = null;
+    for(let i=0;i<frutas.length;i++){
+        elemento = frutas[i];
+        if(fruta === elemento){
+            FrutaEncontrada = elemento;
+            break;
+        }
+    }
+    return FrutaEncontrada;
+}
